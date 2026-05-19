484248a90b47c0a3780561d166a645a8091968d3
ym4889 <m.yamazaki4889@gmail.com>
2026-05-19 15:21:17 +0900

    requestを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,13 +25,15 @@ public class PostController {
     model.addAttribute("postForm", new PostForm());
     return "posts/index";
   }
-  
+
   @PostMapping("/posts")
-  public String savePost(@ModelAttribute("postForm") PostForm form){
+  public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form){
     PostEntity post = new PostEntity();
     post.setContent(form.getContent());
     postRepository.insert(post);
-    return "redirect:/";
+    System.out.println(post);
+    PostEntity resultPost = postRepository.findById(post.getId());
+    return ResponseEntity.ok(resultPost);
   }
   
 }
